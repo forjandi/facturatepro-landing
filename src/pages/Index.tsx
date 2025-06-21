93800b6e28188fdd7a8b6e63823eaffeb3ba6cc4
esmithas <esmith_as@hotmail.com>
2025-06-21 17:16:53 -0500

    mend

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,7 +6,7 @@ import WhyChooseUs from '../components/WhyChooseUs';
 import Testimonials from '../components/Testimonials';
 import FAQ from '../components/FAQ';
 import Footer from '../components/Footer';
-import AboutSystem from '@/components/AboutSytem';
+import AboutSystem from '@/components/AboutSystem';
 
 const Index = () => {
   const [isLoading, setIsLoading] = useState(true);
